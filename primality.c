d01547da1eea4b48ff25dadbf2661f4ec91c979b
Anderson H. da Rosa <andersonhrosa@outlook.com>
2022-09-26 20:22:02 -0300

    second commmit

--- primality.c
+++ primality.c
@@ -1,39 +1,38 @@
 #include <stdio.h>
- 
-int main (int argc, char *argv[])
+
+int main(int argc, char *argv[])
 {
- 
- int val;
- 
- printf ("Digite um número:");
- scanf ("%d", &val);
- 
- ePrimo(val);
- 
+
+    int val;
+
+    printf("Digite um número:");
+    scanf("%d", &val);
+
+    ePrimo(val);
 }
- 
-int ePrimo (int valor)
+
+int ePrimo(int valor)
 {
- int limite, cur;
- int teste= 0;
- 
- limite = (valor/2) + 1;
- cur = 2;
- while (teste != 1 && cur < limite)
- {
- if (valor % cur == 0)
- {
- teste = 1;
- }
- cur = cur + 1 ;
- }
- 
- if (teste == 1)
- {
- printf ("%d não é um número primo!", valor); 
- }else
- {
- printf ("%d é um número primo!", valor); 
- }
- 
+    int limite, cur;
+    int teste = 0;
+
+    limite = (valor / 2) + 1;
+    cur = 2;
+    while (teste != 1 && cur < limite)
+    {
+        if (valor % cur == 0)
+        {
+            teste = 1;
+        }
+        cur = cur + 1;
+    }
+
+    if (teste == 1)
+    {
+        printf("%d não é um número primo!", valor);
+    }
+    else
+    {
+        printf("%d é um número primo!", valor);
+    }
 }
